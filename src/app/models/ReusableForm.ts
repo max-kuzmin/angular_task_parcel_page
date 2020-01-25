40dc3910be5b299ea2d06796f41d1523b12f3e72
Max Kuzmin <max_kuzmin@outlook.com>
2020-01-25 09:58:13 +0300

    Code formatting

--- src/app/models/ReusableForm.ts
+++ src/app/models/ReusableForm.ts
@@ -10,59 +10,60 @@ import {
 import { Subscription } from 'rxjs';
 
 export class ReusableForm<T> implements ControlValueAccessor, OnDestroy {
-    form: FormGroup;
-    subscriptions: Subscription[] = [];
-    onChange: any = () => {};
-    onTouched: any = () => {};
+  form: FormGroup;
+  subscriptions: Subscription[] = [];
+  onChange: any = () => { };
+  onTouched: any = () => { };
 
-    constructor(private formBuilder: FormBuilder, controlsConfig) {
-        this.form = this.formBuilder.group(controlsConfig);
-        this.subscriptions.push(
-          this.form.valueChanges.subscribe(value => {
-            this.onChange(value);
-            this.onTouched();
-          })
-        );
-      }
-
-    get value(): T {
-        return this.form.value;
-      }
-    
-      set value(value: T) {
-        this.form.setValue(value);
+  constructor(private formBuilder: FormBuilder, controlsConfig) {
+    this.form = this.formBuilder.group(controlsConfig);
+    this.subscriptions.push(
+      this.form.valueChanges.subscribe(value => {
         this.onChange(value);
         this.onTouched();
-      }
+      })
+    );
+  }
 
-    ngOnDestroy() {
-        this.subscriptions.forEach(s => s.unsubscribe());
-      }
-    
-      registerOnChange(fn) {
-        this.onChange = fn;
-      }
-    
-      writeValue(value: T) {
-        if (value) {
-          this.value = value;
-        }
-    
-        if (value === null) {
-          this.form.reset();
-        }
-      }
-    
-      registerOnTouched(fn) {
-        this.onTouched = fn;
-      }
-    
-      validate(_: FormControl) {
-        return this.form.valid ? null : { value: { valid: false } };
-      }
+  get value(): T {
+    return this.form.value;
+  }
+
+  set value(value: T) {
+    this.form.setValue(value);
+    this.onChange(value);
+    this.onTouched();
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(s => s.unsubscribe());
+  }
+
+  registerOnChange(fn) {
+    this.onChange = fn;
+  }
+
+  writeValue(value: T) {
+    if (value) {
+      this.value = value;
+    }
+
+    if (value === null) {
+      this.form.reset();
+    }
+  }
+
+  registerOnTouched(fn) {
+    this.onTouched = fn;
+  }
+
+  validate(_: FormControl) {
+    return this.form.valid ? null : { value: { valid: false } };
+  }
 }
 
-export function CreateProviders<T>(component: T) { return [
+export function CreateProviders<T>(component: T) {
+  return [
     {
       provide: NG_VALUE_ACCESSOR,
       useExisting: forwardRef(() => component),
@@ -73,4 +74,5 @@ export function CreateProviders<T>(component: T) { return [
       useExisting: forwardRef(() => component),
       multi: true
     }
-  ];}
+  ];
+}
--- src/app/tracking-number-field/tracking-number-field.component.ts
+++ src/app/tracking-number-field/tracking-number-field.component.ts
@@ -13,14 +13,14 @@ export interface ITrackingNumberFieldValues {
   selector: 'app-tracking-number-field',
   templateUrl: './tracking-number-field.component.html',
   styleUrls: ['./tracking-number-field.component.css'],
-  providers : CreateProviders(TrackingNumberFieldComponent)
+  providers: CreateProviders(TrackingNumberFieldComponent)
 })
 export class TrackingNumberFieldComponent extends ReusableForm<ITrackingNumberFieldValues> {
   hasErrors() {
     return this.form.controls.value.touched && this.form.controls.value.hasError('required')
   }
 
-  constructor(formBuilder: FormBuilder) { 
+  constructor(formBuilder: FormBuilder) {
     super(formBuilder, {
       value: ['', Validators.required]
     });
